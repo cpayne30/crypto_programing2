ab9a62d5bd7e1fd6457c5e59fe023210f8f1a30f
denizhanpak <dpak1@vols.utk.edu>
2017-11-07 18:26:13 -0500

    Fixed keygen

--- rsa-keygen.py
+++ rsa-keygen.py
@@ -11,8 +11,27 @@ def check_arg(args=None):
     results = parser.parse_args(args)
 return (results.public, results.secret, results.number)
 
+def multinv(order, value):
+    x, y = 0, 1
+    a, b = order, value
 
-#def rsa_keygen():
+    while b != 0:
+        a, b, c = b, a % b, a // b
+        x, y = y - c * x, x
+        rv = (1 - y * order) // value
+    if rv < 0:
+        rv += order
+
+    return rv
+
+def rsa_keygen(num_bits):
+    prime1 = randprime(num_bits)
+    prime2 = randprime(num_bits)
+    N = prime1 * prime2
+    order = (prime - 1) * (prime2 - 1)
+    e = 7
+    d = multinv(order, e)
+    return [N, e, d]
 
 
 p, s, n = check_arg(sys.argv[1:])
