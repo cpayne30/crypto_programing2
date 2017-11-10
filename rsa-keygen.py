96bbcab571426f2421dc853de5f9d1d1b8a7f9f3
Zac <ztrzil@vols.utk.edu>
2017-11-10 14:35:05 -0500

    unfucking things

--- rsa-keygen.py
+++ rsa-keygen.py
@@ -1,65 +1,41 @@
 #!/usr/bin/env python3
 import binascii, pip, sys, argparse, random
 
+
 #Parses Arguments
-'''
 def check_arg(args=None):
-  parser = argparse.ArgumentParser(description='Process flags for the program.')
+    parser = argparse.ArgumentParser(description='Process flags for the program.')
     parser.add_argument('-p','--public', required= 'True', help='public key file')
     parser.add_argument('-s','--secret', required= 'True', help='secret key file')
     parser.add_argument('-n','--number', required= 'True', help='number of bits')
     results = parser.parse_args(args)
-return (results.public, results.secret, results.number)
-'''
+    return (results.public, results.secret, results.number)
 
-#def rsa_keygen():
+def multinv(order, value):
+    x, y = 0, 1
+    a, b = order, value
 
-def ProbPrimeFermat(x):
-  return 2**(x - 1) % x == 1
+    while b != 0:
+        a, b, c = b, a % b, a // b
+        x, y = y - c * x, x
+        rv = (1 - y * order) // value
+    if rv < 0:
+        rv += order
 
-def ProbPrimeRabinMiller(n):
-  """Return True if n passes k rounds of the Miller-Rabin primality
-    test (and is probably prime). Return False if n is proved to be
-    composite.
-    """
-  small_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
-  k = 4
-  if n < 2: return False
-  for p in small_primes:
-    if n < p * p: return True
-    if n % p == 0: return False
-  r, s = 0, n - 1
-  while s % 2 == 0:
-    r += 1
-    s //= 2
-  for _ in range(k):
-    a = random.randrange(2, n - 1)
-    x = pow(a, s, n)
-    if x == 1 or x == n - 1:
-      continue
-    for _ in range(r - 1):
-      x = pow(x, 2, n)
-      if x == n - 1:
-        break
-    else:
-      return False
-  return True
+    return rv
 
-def prime_gen(bit_len):
-  while True:
-    prime = random.getrandbits(bit_len)
-    prime = prime | bit_len
+def rsa_keygen(num_bits):
+    prime1 = randprime(num_bits)
+    prime2 = randprime(num_bits)
+    N = prime1 * prime2
+    order = (prime - 1) * (prime2 - 1)
+    e = 7
+    d = multinv(order, e)
+    return [N, e, d]
 
-    if ProbPrimeFermat(prime) and ProbPrimeRabinMiller(prime): break
 
-  return prime
+p, s, n = check_arg(sys.argv[1:])
 
-#p, s, n = check_arg(sys.argv[1:])
+public_key = open(p, 'rb').read()#Does this need to be 'rb' instead of 'r'? Doesn't matter but "rb" is faster.
 
-#public_key = open(p, 'r').read()#Does this need to be 'rb' instead of 'r'?
-
-#private_key = open(s, 'rb').read()
-
-for i in range (0, 100):
-  rv = prime_gen(24)
-  print (bin(rv), rv)
+private_key = open(s, 'rb').read()
